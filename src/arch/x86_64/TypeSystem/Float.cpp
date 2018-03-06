d448f46eb91aaf308a0abc8a255f4a65468f061c
jonay2000 <jonabent@gmail.com>
2018-03-06 11:51:35 +0100

    baic float maths implemented too

--- src/arch/x86_64/TypeSystem/Float.cpp
+++ src/arch/x86_64/TypeSystem/Float.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 using namespace std;
 
 #include <Float.h>
@@ -14,20 +15,64 @@ string Float::__type__(){
 }
 
 BaseObject * Float::__add__(BaseObject * other){
-
 	if(other->__type__() == "Integer"){
 		return new Float(this->val + (double)(dynamic_cast<Integer *>(other)->val));
 	}else if(other->__type__() == "Float"){
 		return new Float(this->val + (double)(dynamic_cast<Float *>(other)->val));
 	}else{
-		exit(1);
+		exit(1); //error handler
 	}
 }
-BaseObject * Float::__sub__(BaseObject * other){return new Float();}
-BaseObject * Float::__mul__(BaseObject * other){return new Float();}
-BaseObject * Float::__div__(BaseObject * other){return new Float();}
-BaseObject * Float::__pow__(BaseObject * other){return new Float();}
-BaseObject * Float::__mod__(BaseObject * other){return new Float();}
+
+BaseObject * Float::__sub__(BaseObject * other){
+	if(other->__type__() == "Integer"){
+		return new Float(this->val - (double)(dynamic_cast<Integer *>(other)->val));
+	}else if(other->__type__() == "Float"){
+		return new Float(this->val - (double)(dynamic_cast<Float *>(other)->val));
+	}else{
+		exit(1); //error handler
+	}
+}
+
+BaseObject * Float::__mul__(BaseObject * other){
+	if(other->__type__() == "Integer"){
+		return new Float(this->val * (double)(dynamic_cast<Integer *>(other)->val));
+	}else if(other->__type__() == "Float"){
+		return new Float(this->val * (double)(dynamic_cast<Float *>(other)->val));
+	}else{
+		exit(1); //error handler
+	}
+}
+
+BaseObject * Float::__div__(BaseObject * other){
+	if(other->__type__() == "Integer"){
+		return new Float(this->val / (double)(dynamic_cast<Integer *>(other)->val));
+	}else if(other->__type__() == "Float"){
+		return new Float(this->val / (double)(dynamic_cast<Float *>(other)->val));
+	}else{
+		exit(1); //error handler
+	}
+}
+
+BaseObject * Float::__pow__(BaseObject * other){
+	if(other->__type__() == "Integer"){
+		return new Float(pow(this->val, (double)(dynamic_cast<Integer *>(other)->val)));
+	}else if(other->__type__() == "Float"){
+		return new Float(pow(this->val, (double)(dynamic_cast<Float *>(other)->val)));
+	}else{
+		exit(1); //error handler
+	}
+}
+
+BaseObject * Float::__mod__(BaseObject * other){
+	if(other->__type__() == "Integer"){
+		return new Float(this->val % (double)(dynamic_cast<Integer *>(other)->val));
+	}else if(other->__type__() == "Float"){
+		exit(1); //error handler
+	}else{
+		exit(1); //error handler
+	}
+}
 
 BaseObject * Float::__copy__(){
 	return new Float(this->val);
